aa53a1cd771869296b2157cab37e6ea54cac9ead
Pham Ngoc Tung <tungpham2127@gmail.com>
2020-10-23 00:34:53 +0700

    Thiết kế các layout edit thông tin cho website người dùng

--- src/main/java/fpt/java/finalproject/controllers/AdminController.java
+++ src/main/java/fpt/java/finalproject/controllers/AdminController.java
@@ -5,13 +5,63 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AdminController {
-    @RequestMapping("/cha")
-    public String cha() {
-        return "test/layout.html";
+    @RequestMapping("/addOrEdit")
+    public String layoutChung() {
+        return "admin/layouts/add_or_edit_layout";
     }
 
-    @RequestMapping("/con")
-    public String con() {
-        return "test/giao_dien.html";
+    // -----------------------------------------------
+    @RequestMapping("/addProduct")
+    public String addProduct() {
+        return "admin/products/add_product";
+    }
+
+    @RequestMapping("/productList")
+    public String productList() {
+        return "admin/products/product_list";
+    }
+
+    // -----------------------------------------------
+    @RequestMapping("/addEmployee")
+    public String addEmployee() {
+        return "admin/employees/add_employee";
+    }
+
+    @RequestMapping("/employeeList")
+    public String EmployeeList() {
+        return "admin/employees/employee_list";
+    }
+
+    // -----------------------------------------------
+    @RequestMapping("/addItem")
+    public String addItem() {
+        return "admin/items/add_item";
+    }
+
+    @RequestMapping("/itemList")
+    public String itemList() {
+        return "admin/items/item_list";
+    }
+
+    // -----------------------------------------------
+    @RequestMapping("/editShop")
+    public String editShop() {
+        return "admin/shops/edit_shop";
+    }
+
+    @RequestMapping("/shopList")
+    public String shopList() {
+        return "admin/shops/shop_list";
+    }
+
+    // -----------------------------------------------
+    @RequestMapping("/editUser")
+    public String editUser() {
+        return "admin/users/edit_user";
+    }
+
+    @RequestMapping("/userList")
+    public String userList() {
+        return "admin/users/user_list";
     }
 }
