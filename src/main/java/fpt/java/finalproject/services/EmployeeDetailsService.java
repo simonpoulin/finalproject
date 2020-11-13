77a5e4e5f5b4133d61a9dacf3b746e0ce5abeb2a
callmerey <levii.reys@gmail.com>
2020-11-13 22:22:50 +0700

    ok

--- src/main/java/fpt/java/finalproject/services/EmployeeDetailsService.java
+++ src/main/java/fpt/java/finalproject/services/EmployeeDetailsService.java
@@ -1,7 +1,10 @@
 package fpt.java.finalproject.services;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface EmployeeDetailsService extends UserDetailsService {
     
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
